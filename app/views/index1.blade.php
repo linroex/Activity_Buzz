da11ea84fb4dd469aaf0ee1588f8f5b7d0989ded
linroex <linroex@coder.tw>
2014-05-25 13:53:12 +0800

    demo site 美化

--- app/views/index1.blade.php
+++ app/views/index1.blade.php
@@ -2,8 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>登入</title>
+    <title>Activity Buzz</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="http://connect.facebook.net/en_US/sdk.js"></script>
     <script>
         userID = '';
@@ -16,8 +18,8 @@
             FB.getLoginStatus(function(res){
                 if(res.status == 'connected'){
                     userID = res.authResponse.userID;
-                    $("#user_tag_link").attr('href',$("#user_tag_link").attr('href') + '/tag1?id=' + userID);
-                    $("#suggest_user_activity_link").attr('href',$("#suggest_user_activity_link").attr('href') + '/like1?id=' + userID);
+                    // $("#user_tag_link").attr('href',$("#user_tag_link").attr('href') + '/tag1?id=' + userID);
+                    // $("#suggest_user_activity_link").attr('href',$("#suggest_user_activity_link").attr('href') + '/like1?id=' + userID);
                     
                 }
             });
@@ -28,15 +30,17 @@
                 FB.getLoginStatus(function(res){
                     if(res.status != 'connected'){
                         FB.login(function(res){
+                            $('#login_btn').text('登出');
                             userID = res.authResponse.userID;
                             register();
                             alert('登入成功');
-                            $("#user_tag_link").attr('href',$("#user_tag_link").attr('href') + '/tag1?id=' + userID);
-                            $("#suggest_user_activity_link").attr('href',$("#suggest_user_activity_link").attr('href') + '/like1?id=' + userID);
+                            // $("#user_tag_link").attr('href',$("#user_tag_link").attr('href') + '/tag1?id=' + userID);
+                            // $("#suggest_user_activity_link").attr('href',$("#suggest_user_activity_link").attr('href') + '/like1?id=' + userID);
                         },{scope:'user_events,user_groups,user_activities,email'});
                     }else{
-                        userID = res.authResponse.userID;
-                        alert('已登入');
+                        FB.logout();
+                        alert('已登出');
+                        $('#login_btn').text('登入');
                     }
                 });
             })  
@@ -47,10 +51,20 @@
                 get_old_event('',0);
                 
             })
-            $('#logout_btn').click(function(){
-                FB.logout();
+            $('#user_tag_link').click(function(e){
+                e.preventDefault();
+                $.get('{{url("tag1?id=")}}'+userID,function(data){
+                    $('#content').text('');
+                    $('#content').append(data);
+                })
             })
-            
+            $('#suggest_user_activity_link').click(function(e){
+                e.preventDefault();
+                $.get('{{url("like1?id=")}}'+userID,function(data){
+                    $('#content').text('');
+                    $('#content').append(data);
+                })
+            })
         });
         function register(){
             FB.api('/me',function(res){
@@ -91,10 +105,49 @@
     </script>
 </head>
 <body>
-    <input type="button" id="login_btn" value="登入">
-    <input type="button" id="logout_btn" value="登出"><br>
-    <input type="button" id="get_btn" value="取得Tag"><br>
-    <a href='{{url()}}' target="_blank" id="user_tag_link">檢視用戶標籤</a><br>
-    <a href='{{url()}}' target="_blank" id="suggest_user_activity_link">檢視推薦貼文</a>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Activity Buzz</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li><a href="#">關於</a></li>
+            
+          </ul>
+          <div class="nav navbar-nav pull-right">
+              <li><a id="login_btn" class="btn">登入</a></li>
+          </div>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Activity Buzz</h1>
+            <p>貼心幫你關注大小活動</p>
+            <input type="button" class="btn btn-default btn-lg " id="get_btn" value="取得Tag">
+        </div>
+        <!-- <input type="button" id="login_btn" value="登入">
+        <input type="button" id="logout_btn" value="登出"><br> -->
+        
+        <div class="btn-group">
+            <a href='{{url()}}' target="_blank" class="btn btn-success btn-lg" id="user_tag_link">檢視用戶標籤</a>
+            <a href='{{url()}}' target="_blank" class="btn btn-primary btn-lg" id="suggest_user_activity_link">檢視推薦貼文</a>
+        </div>
+        <div id="content">
+            
+        </div>
+    </div>
+    
 </body>
 </html>
